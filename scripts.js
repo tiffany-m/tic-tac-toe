89f8eaf71a04cc1615f708be0f012a96f61da2ea
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-08-23 15:33:46 -0400

    Initial gameboard logic

--- scripts.js
+++ scripts.js
@@ -3,8 +3,28 @@ const players = (sign) = {
 }
 
 let gameBoard = (() => {
-    const board = ["1", "1", "1", "1", "1", "1", "1", "1", "1"];
+    const board = ["", "", "", "", "", "", "", "", ""];
+    const restartBtn = document.getElementById('restart-btn');
+    let squares = document.querySelectorAll(".box");
+    let squaresArray = Array.from(squares);
+
+    function clearBoard() {
+        for(let i = 0; i < board.length; i++) {
+            squaresArray[i].textContent = "";
+        }
+    }
+
+    squares.forEach(square => square.addEventListener("click", (e) => {
+        if(e.target.innerText == "") {
+            square.innerText = "O";
+        } 
+    }))
+
+    restartBtn.addEventListener("click", () => {
+        clearBoard()
+    });
 })(); 
+
 
 
 
