84b94fcdfa0f36c3377f7cee15fde8f957297daa
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-08-23 15:39:06 -0400

    Add logic for going back and forth between players

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ let gameBoard = (() => {
     const restartBtn = document.getElementById('restart-btn');
     let squares = document.querySelectorAll(".box");
     let squaresArray = Array.from(squares);
+    let currentPlayer = "X";
 
     function clearBoard() {
         for(let i = 0; i < board.length; i++) {
@@ -15,9 +16,15 @@ let gameBoard = (() => {
     }
 
     squares.forEach(square => square.addEventListener("click", (e) => {
-        if(e.target.innerText == "") {
+        if(e.target.innerText == "X" || e.target.innerText == "O") {
+            return;
+        } else if (currentPlayer == "X") {
+            square.innerText = "X"
+            currentPlayer = "O";
+        } else {
             square.innerText = "O";
-        } 
+            currentPlayer = "X";
+        }
     }))
 
     restartBtn.addEventListener("click", () => {
