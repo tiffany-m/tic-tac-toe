8b6d0009ded925a31c74f87039ccce781e7ffe47
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-08-24 14:15:40 -0400

    Logic for determining if there is a winner

--- scripts.js
+++ scripts.js
@@ -6,13 +6,37 @@ let gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
     const restartBtn = document.getElementById('restart-btn');
     const squares = document.querySelectorAll(".box");
-    let squaresArray = Array.from(squares);
+    const squaresArray = Array.from(squares);
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
     let currentPlayer = "X";
+    let winner = "";
 
     function clearBoard() {
         for(let i = 0; i < board.length; i++) {
             squaresArray[i].textContent = "";
+            board[i] = "";
         }
+        currentPlayer = "X";
+    }
+
+    function checkForWinner() {
+        for (let i = 0; i < winningConditions.length; i++) {
+            const [a, b, c] = winningConditions[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                console.log("win");
+                return board[a]; // Returns the winning player ("X" or "O")
+            }
+        }
+        return null; 
     }
 
     squares.forEach(square => square.addEventListener("click", (e) => {
@@ -31,6 +55,8 @@ let gameBoard = (() => {
             square.innerText = "O";
             currentPlayer = "X";
         }
+
+        winner = checkForWinner();
     }))
 
     restartBtn.addEventListener("click", () => {
